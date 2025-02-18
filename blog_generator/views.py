6f0c61c6df5668f3b62dcfb77a153ea93ee3c604
ldravidu-wsl <ldravidu@gmail.com>
2025-02-18 17:19:16 +0530

    Implement user signup functionality and minor bug fix
    - Implement user signup
    - Fix password input displaying as text by changing input type to
    'password'

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def index(request):
@@ -8,6 +10,25 @@ def user_login(request):
     return render(request, 'login.html')
 
 def user_signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirmPassword = request.POST['confirmPassword']
+
+        if password == confirmPassword:
+            try:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                login(request, user)
+                return redirect('/')
+            except:
+                error_message = 'Error creating account'
+                return render(request, 'signup.html', {'error_message': error_message})
+
+        else:
+            error_message = 'Passwords do not match'
+            return render(request, 'signup.html', {'error_message': error_message})
     return render(request, 'signup.html')
 
 def user_logout(request):
