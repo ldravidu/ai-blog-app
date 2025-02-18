2f84fb072dc6da5ceea4cf950c11f71637f00be1
ldravidu-wsl <ldravidu@gmail.com>
2025-02-18 18:08:00 +0530

    Implement user login and logout functionality

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -1,12 +1,25 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/login')
 def index(request):
     return render(request, 'index.html')
 
 def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = 'Invalid credentials'
+            return render(request, 'login.html', {'error_message': error_message})
     return render(request, 'login.html')
 
 def user_signup(request):
@@ -32,4 +45,5 @@ def user_signup(request):
     return render(request, 'signup.html')
 
 def user_logout(request):
-    pass
+    logout(request)
+    return redirect('/login')
